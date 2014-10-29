9c05b7bcb58ce16ece1e03f9907eb936564a66a2
Hilmar Geir Eiðsson <hge2@hi.is>
2014-10-29 02:27:26 +0000

    Input for player names

--- src/main/java/is/ru/tictactoe/GameLogic.java
+++ src/main/java/is/ru/tictactoe/GameLogic.java
@@ -1,11 +1,22 @@
 package is.ru.tictactoe;
 
+import java.util.Scanner;
 
 public class GameLogic {
 
+	public static Board b = new Board();
+	public static Scanner in = new Scanner(System.in);
+	
+	public static Player one = new Player("", 1);
+	public static Player two = new Player("",2);
+
 	public static void main(String [] args)
 	{
-		Player leikmadur = new Player("Nonni", 1);
-		System.out.println(leikmadur.getName());
+		one.setName(in.nextLine());
+		two.setName(in.nextLine());
+
+		System.out.println(one.getName());
+		System.out.println(two.getName());
+			
 	}
 }
