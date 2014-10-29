ed97377379e6faf96a938a0f9c3d191f675cea71
jonj12 <jonj12@ru.is>
2014-10-29 01:31:17 +0000

    Player class with constructor, getters and setters for name and ID

--- src/main/java/is/ru/tictactoe/GameLogic.java
+++ src/main/java/is/ru/tictactoe/GameLogic.java
@@ -1,20 +1,11 @@
 package is.ru.tictactoe;
 
- 
-/*import java.io.*;
-import java.lang.System.*;
-import java.util.Scanner;
-*/
-public class GameLogic {
 
-	//Board board = new Board();
-	//private Player x = new Player();	
-	//private Player o = new Player();	
-	
-	
+public class GameLogic {
 
 	public static void main(String [] args)
 	{
-		System.out.println("TEST");
+		Player leikmadur = new Player("Nonni", 1);
+		System.out.println(leikmadur.getName());
 	}
 }
--- src/main/java/is/ru/tictactoe/Player.java
+++ src/main/java/is/ru/tictactoe/Player.java
@@ -2,6 +2,28 @@ package is.ru.tictactoe;
 
 
 public class Player {
+	String nameOfPlayer;
+	int playerID;
 
+	public Player(String name, int pID){
+		nameOfPlayer = name;
+		playerID = pID;
+	}
+
+	public String getName(){
+		return nameOfPlayer;
+	}
+
+	public void setName(String name){
+		nameOfPlayer = name;
+	}
+
+	public int getPlayerID(){
+		return playerID;
+	}
+
+	public void setPlayerID(int pID){
+		playerID = pID;
+	}
 
 }
