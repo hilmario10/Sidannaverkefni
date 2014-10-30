178db5a83896a640d1dee010ad2f15668d8dd8da
jonj12 <jonj12@ru.is>
2014-10-30 12:54:15 +0000

    Testing features in main funtion for print Board

--- src/main/java/is/ru/tictactoe/GameLogic.java
+++ src/main/java/is/ru/tictactoe/GameLogic.java
@@ -7,8 +7,8 @@ public class GameLogic {
 	public static Board b = new Board();
 	public static Scanner in = new Scanner(System.in);
 	
-	public static Player one = new Player("", 1);
-	public static Player two = new Player("",2);
+	public static Player one = new Player("", 0);
+	public static Player two = new Player("", 1);
 
 	public static void main(String [] args)
 	{
@@ -17,6 +17,9 @@ public class GameLogic {
 
 		System.out.println(one.getName());
 		System.out.println(two.getName());
+		b.SetMove(one.getPlayerID(), 1, 2);
+		b.SetMove(two.getPlayerID(), 1, 0);
+		b.PrintBoard();
 			
 	}
 }
