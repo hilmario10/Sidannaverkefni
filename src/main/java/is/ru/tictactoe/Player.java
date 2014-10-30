4f7e0b98738ceabe679a86ac9f66a1bab559ba3a
Tonalicious <antonh13@ru.is>
2014-10-30 11:11:55 +0000

    Player.java upgrade

--- src/main/java/is/ru/tictactoe/Player.java
+++ src/main/java/is/ru/tictactoe/Player.java
@@ -1,30 +1,34 @@
 package is.ru.tictactoe;
 
-
 public class Player {
+	
 	//Variables for player.
-	String nameOfPlayer;
-	int playerID;
+	private String nameOfPlayer;
+	private int playerID;
 
+	// Constructor for player.
 	public Player(String name, int pID){
 		nameOfPlayer = name;
 		playerID = pID;
 	}
 
+	// Returns player name.
 	public String getName(){
 		return nameOfPlayer;
 	}
 
+	// Sets player name.
 	public void setName(String name){
 		nameOfPlayer = name;
 	}
 
+	// Returns player ID.
 	public int getPlayerID(){
 		return playerID;
 	}
 
+	// Sets player ID.
 	public void setPlayerID(int pID){
 		playerID = pID;
 	}
-
 }
