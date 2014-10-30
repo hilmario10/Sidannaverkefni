e691fb3753598fc601ccc909f366eb1a2945087b
jonj12 <jonj12@ru.is>
2014-10-30 01:03:01 +0000

    New test for SetMove function

--- src/test/java/is/ru/tictactoe/BoardTest.java
+++ src/test/java/is/ru/tictactoe/BoardTest.java
@@ -10,8 +10,21 @@ import org.junit.Test;
 		public void testLegalMove() {
 			assertEquals(true, Board.LegalMove(1));
 			assertEquals(true, Board.LegalMove(2));
-			assertEquals(true, Board.LegalMove(3));
+			assertEquals(false, Board.LegalMove(3));
 			assertEquals(false, Board.LegalMove(-2));
 			assertEquals(false, Board.LegalMove(4));
-		}		
+		}	
+
+		//Check if move from player is correct
+		@Test
+		public void testSetMove	(){
+			Board newboard = new Board();
+
+			newboard.SetMove(1, 2, 0);
+			newboard.SetMove(0, 1, 2);
+			newboard.SetMove(0, 1, 1);
+			assertEquals(2, newboard.GetStateOfField(2, 0));
+			assertEquals(1, newboard.GetStateOfField(1, 2));
+			assertEquals(1, newboard.GetStateOfField(1, 1));
+		}
 }
