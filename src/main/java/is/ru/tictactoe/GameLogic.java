b0a48efcf5ba85686e26f342102e682837409f09
jonj12 <jonj12@ru.is>
2014-10-30 13:49:56 +0000

    Added NewGame function for runnig the game

--- src/main/java/is/ru/tictactoe/GameLogic.java
+++ src/main/java/is/ru/tictactoe/GameLogic.java
@@ -9,17 +9,43 @@ public class GameLogic {
 	
 	public static Player one = new Player("", 0);
 	public static Player two = new Player("", 1);
+	public static boolean keepPlaying = true;
 
 	public static void main(String [] args)
 	{
+		NewGame();
+	}
+
+	public static void NewGame(){
+		int whichPlayer = 0;
+
+		System.out.println("Player 1 please enter your name");
 		one.setName(in.nextLine());
+		System.out.println("Player 2 please enter your name");
 		two.setName(in.nextLine());
-		
-		System.out.println(one.getName());
-		System.out.println(two.getName());
-		b.SetMove(one.getPlayerID(), 1, 2);
-		b.SetMove(two.getPlayerID(), 1, 0);
-		b.PrintBoard();
-			
+
+		while(keepPlaying){
+			if(whichPlayer == 0){
+				System.out.println(one.getName() + " Please enter your X cord move");
+				int x = in.nextInt();
+				System.out.println(one.getName() + " Please enter your Y cord move");
+				int y = in.nextInt();
+				b.SetMove(one.getPlayerID(), x, y);
+				b.PrintBoard();
+				whichPlayer = 1;
+			}
+			else{
+				System.out.println(two.getName() + " Please enter your X cord move");
+				int x = in.nextInt();
+				System.out.println(two.getName() + " Please enter your Y cord move");
+				int y = in.nextInt();
+				b.SetMove(two.getPlayerID(), x, y);
+				b.PrintBoard();
+				whichPlayer = 0;
+			}
+		}
+		System.out.println("Congrats you have won the game");
+
+
 	}
 }
